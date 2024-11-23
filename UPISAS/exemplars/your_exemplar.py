68dcb650eeb155af9d8f7f06f259c02817b6989a
yoBoyio <dimi044@hotmail.com>
2024-11-23 02:32:42 +0100

    feat: setup env for adaptation strategy

---
+++ UPISAS/exemplars/your_exemplar.py
@@ -0,0 +1,29 @@
+import pprint, time
+from UPISAS.exemplar import Exemplar
+import logging
+pp = pprint.PrettyPrinter(indent=4)
+logging.getLogger().setLevel(logging.INFO)
+
+
+class StepExemplar(Exemplar):
+    """
+    A class which encapsulates a self-adaptive exemplar run in a docker container.
+    """
+
+    def __init__(self, auto_start: " Whether to immediately start the container after creation" =False , container_name = "fafas2"):
+        my_docker_kwargs = {
+            "name":  container_name,
+            "image": "crowdnav2024-http-server",
+            "ports" : {8080: 8080},
+            "network": "fas-net" 
+            }
+
+        super().__init__("http://localhost:8080", my_docker_kwargs, auto_start)
+
+    def start_run(self):
+        self.exemplar_container.exec_run("uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload", detach=True)
+
+    # stop the container if it goes to except
+    def stop(self):
+       self.stop_container()
+
--- UPISAS/strategies/empty_strategy.py
+++ UPISAS/strategies/empty_strategy.py
@@ -1,10 +1,54 @@
 from UPISAS.strategy import Strategy
-
+import gym
+from gym import spaces
+import numpy as np
+import requests
 
 class EmptyStrategy(Strategy):
+    STEP_THRESHOLD = 0.3
+    INITIAL_VALUE = 0.1
 
+    # Initializing the strategy
+    def __init__(self, exemplar):
+        super().__init__(exemplar)
+        # self.Planner = Planner
+        self.data = self.knowledge.monitored_data
+        self.adaptation_options = self.knowledge.adaptation_options
+        self.itial_step = 0
+        self.counter = 0
+        
+    # analyze method
     def analyze(self):
-        return True
+        self.counter += 1
+        car_stats = self.data['car_stats'][-1]
+        configs = self.data['configs'][-1]
+        self.knowledge.analysis_data["exploration_percentage"] = configs["exploration_percentage"]
+        print(self.data['car_stats'],'eeeee')
+        print(car_stats['step'],'step')
+        #  total_trip_average = self.data['total_trip_average']
+        # total_trips = self.data['total_trips']
+        # total_complaints = self.data['total_complaints']
+        step = car_stats['step']
+        self.knowledge.analysis_data["step"] = step
+        
+        if self.counter > 50000:
+            self.sufficient  = True
+            return True
+        self.sufficient  = False
 
+        return False
+        
+    # Planner method 
     def plan(self):
-        return True
+        # step = self.knowledge.analysis_data["step"] 
+        # print(step, 'step')
+        # if step > 50000 :
+        #     self.knowledge.plan_data["exploration_percentage"]  = self.knowledge.analysis_data["exploration_percentage"] + 0.1
+        #     print(self.knowledge.plan_data["exploration_percentage"],'exploration')
+        #     return True
+
+        # return False
+
+        pass
+    
+   
