c2521f004d6c739f7ad6c6cd4a26a4281633387e
r0han0908 <bigd4mher0@gmail.com>
2024-12-20 22:14:42 +0100

    t

--- run.py
+++ run.py
@@ -13,44 +13,53 @@ if __name__ == '__main__':
 
     try:
         
-    #     strategy = EmptyStrategy(exemplar)
-    #     # strategy.analyze()
-    #     strategy.execute()
-    #     # strategy.get_monitor_schema()
-    #     # strategy.get_adaptation_options_schema()
-    #     # strategy.get_execute_schema()
+        strategy = EmptyStrategy(exemplar)
+        strategy.monitor(with_validation=False)
+        time.sleep(10)
 
-    #     while True:
-    #         input("Try to adapt?")
-    #         strategy.monitor(verbose=True)
-    #         if strategy.analyze():
-    #             if strategy.plan():
-    #                 strategy.execute(strategy.knowledge.plan_data,with_validation=False)
+        values = len(strategy.knowledge.monitored_data["tripOverhead"])
 
-    max_iterations = 1000
-    for iteration in range(max_iterations):
-        # MONITOR: Gather traffic metrics
-        trip_overhead, trip_average = monitor()
+        if values >= 40:
+            strategy.analyze()
+            strategy.execute()
+        else:
+            print("Not enough data to analyze and execute")
+        
+        # strategy.get_monitor_schema()
+        # strategy.get_adaptation_options_schema()
+        # strategy.get_execute_schema()
 
-        # Calculate current reward
-        reward = calculate_reward(trip_overhead, trip_average)
+        while True:
+            input("Try to adapt?")
+            strategy.monitor(verbose=True)
+            if strategy.analyze():
+                if strategy.plan():
+                    strategy.execute(strategy.knowledge.plan_data,with_validation=False)
 
-        # ANALYZE: Check performance and update feedback count
-        adjust_parameters, decline_count = analyze(reward, decline_count, reward_threshold, decline_limit)
+    # max_iterations = 1000
+    # for iteration in range(max_iterations):
+    #     # MONITOR: Gather traffic metrics
+    #     trip_overhead, trip_average = monitor()
 
-        # PLAN: Calculate UCB scores and select the best arm
-        ucb_scores = calculate_ucb(rewards, counts, t)
-        selected_arm, current_config = plan(ucb_scores, rewards, counts, t, arms, decline_count)
+    #     # Calculate current reward
+    #     reward = calculate_reward(trip_overhead, trip_average)
 
-        # EXECUTE: Apply the selected configuration
-        EmptyStrategy.execute(EmptyStrategy, current_config, selected_arm, EmptyStrategy.rewards, EmptyStrategy.counts, reward, EmptyStrategy)
+    #     # ANALYZE: Check performance and update feedback count
+    #     adjust_parameters, decline_count = analyze(reward, decline_count, reward_threshold, decline_limit)
+
+    #     # PLAN: Calculate UCB scores and select the best arm
+    #     ucb_scores = calculate_ucb(rewards, counts, t)
+    #     selected_arm, current_config = plan(ucb_scores, rewards, counts, t, arms, decline_count)
+
+    #     # EXECUTE: Apply the selected configuration
+    #     EmptyStrategy.execute(EmptyStrategy, current_config, selected_arm, EmptyStrategy.rewards, EmptyStrategy.counts, reward, EmptyStrategy)
         
 
-        # KNOWLEDGE: Log system performance
-        log_performance(iteration, reward, current_config)
+    #     # KNOWLEDGE: Log system performance
+    #     log_performance(iteration, reward, current_config)
 
-        # Increment timestep
-        t += 1
+    #     # Increment timestep
+    #     t += 1
             
     except (Exception, KeyboardInterrupt) as e:
         print(str(e))
