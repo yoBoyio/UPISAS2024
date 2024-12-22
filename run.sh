11430da26aae877cb8416b3d0c13b7702fb57a97
r0han0908 <bigd4mher0@gmail.com>
2024-12-22 01:19:02 +0100

    Script change

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/UPISAS
-python3 experiment-runner/experiment-runner/ UPISAS/experiment_runner_configs/Crowdnav_exp.py
+python3 UPISAS/experiment_runner_configs/Crowdnav_exp.py
